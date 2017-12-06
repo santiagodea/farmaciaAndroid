c929e5c70dfe181385d40fef19f487029d920eb4
Santiago <sntgdeandrea@gmail.com>
2017-12-06 18:13:15 -0300

    inicio

---
+++ app/src/main/java/ar/com/ciu/pharmapp/PharmApp.java
@@ -0,0 +1,13 @@
+package ar.com.ciu.pharmapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PharmApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pharm_app);
+    }
+}
