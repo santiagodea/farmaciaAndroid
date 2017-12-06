2b8f2c18b51131733276ad56ca227f8c21c904bd
Santiago <sntgdeandrea@gmail.com>
2017-12-06 18:50:25 -0300

    agregados dos layout

---
+++ app/src/main/java/ar/com/ciu/pharmapp/PharmacyList.java
@@ -0,0 +1,13 @@
+package ar.com.ciu.pharmapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PharmacyList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pharmacy_list);
+    }
+}
