6a879aef7ad82b8ef5be1d69d64ef595caea9279
Santiago <sntgdeandrea@gmail.com>
2017-12-12 19:35:28 -0300

    agregado PharmacyDataObject

--- app/src/main/java/ar/com/ciu/pharmapp/PharmacyListPack/PharmacyData.java
+++
@@ -1,8 +0,0 @@
-package ar.com.ciu.pharmapp.PharmacyListPack;
-
-/**
- * Created by santi on 6/12/2017.
- */
-
-public class PharmacyData {
-}
---
+++ app/src/main/java/ar/com/ciu/pharmapp/PharmacyListPack/PharmacyDataObject.java
@@ -0,0 +1,33 @@
+package ar.com.ciu.pharmapp.PharmacyListPack;
+
+/**
+ * Created by santi on 6/12/2017.
+ */
+
+public class PharmacyDataObject {
+    private String name;
+    private String address;
+    private Integer landphone;
+    private Integer alternativePhone;
+
+    public PharmacyDataObject(String name, String address, Integer landPhone, Integer alternativePhone) {
+        super();
+        this.name = name;
+        this.address = address;
+        this.landphone = landPhone;
+        this.alternativePhone = alternativePhone;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public Integer getLandphone() {
+        return landphone;
+    }
+    public Integer getAlternativePhone() {
+        return alternativePhone;
+    }
+}
